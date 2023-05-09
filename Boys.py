99f3b619b48c6457f9eb98feba4501c4b457b124
SleepySyzes <132785165+SleepySyzes@users.noreply.github.com>
2023-05-09 20:08:41 +0100

    Update Boys.py

--- Boys.py
+++ Boys.py
@@ -14,34 +14,45 @@ if a == "forward" or a == "Forward":
                 data = True
                 #health is the opponents health
                 health = 200
-                #helth2 is your health
+                #health2 is your health
                 health2 = 200
                 while data:
+                    g = [ random.randint(1, 2) for _ in range(1) ]
                     d = [ random.randint(1, 2) for _ in range(1) ]
+                    f = [ random.randint(1, 3) for _ in range(1) ]
                     a = str(input("Fight!    Punch:50 (50%)    Kick:75 (33%)"))
                     if health <= 0:
                         data = False
                         z = str(input("You run away with the loot you have gathered from the body."))
                         #This is where the story carries on if you win the fight
                     elif a == "Punch" or a == "punch":
-                       b = [ random.randint(1, 2) for _ in range(1) ]
-                    if b == [1]:
-                        health = health - 50
-                    elif b == [2]:
+                        b = [ random.randint(1, 2) for _ in range(1) ]
+                        if b == [1]:
+                            health = health - 50
+                            print ("You punched him!")
+                        elif b == [2]:
                            health = health
                     elif a == "Kick" or a == "kick":
                         c = [ random.randint(1, 3) for _ in range(1) ]
-                    if c == [1]:
-                        health = health - 75
-                    elif c == [2]:
-                        health = health
+                        if c == [1]:
+                            health = health - 75
+                            print ("You kicked him!")
+                        elif c == [2]:
+                            health = health
+                    if g == [1]:
+                        if d == [1]:
+                            health2 = health2 - 40
+                            print("You were punched! Your health is now:",health2)
+                        elif d == [2]:
+                            health2 = health2
+                    if g == [2]:    
+                        if f == [1]:
+                            health2 = health2 - 60
+                            print("You were kicked! Your health is now:",health2)
+                        elif f == [2] or f == [3]:
+                            health2 = health2
                     if health2 <= 0:
                         data = False
                         e = print("You lost the battle. You have lost the hunger games.")
                         #This is when you die
-                    elif d == [1]:
-                        health2 = health2 - 50
-                        print("You were punched! Your health is now:",health2)
-                    elif d == [2]:
-                        health2 = health2
                     print ("Opponents health:",health)
